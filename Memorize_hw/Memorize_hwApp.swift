9ef0b2992bf974753fd36640e10b54f3eaa964a4
Gwen-JW <jiawennw@gmail.com>
2021-09-27 14:31:13 +0800

    Initial Commit

---
+++ Memorize_hw/Memorize_hwApp.swift
@@ -0,0 +1,17 @@
+//
+//  Memorize_hwApp.swift
+//  Memorize_hw
+//
+//  Created by Jiawen Wei on 2021/9/27.
+//
+
+import SwiftUI
+
+@main
+struct Memorize_hwApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
